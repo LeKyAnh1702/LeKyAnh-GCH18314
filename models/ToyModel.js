55804f120d21518a961977094cfd389a812e3a96
LeKyAnh1702 <lekyanhsd@gmail.com>
2023-08-20 14:56:57 +0700

    assignment

---
+++ models/ToyModel.js
@@ -0,0 +1,11 @@
+var mongoose = require ('mongoose');
+var ToySchema = mongoose.Schema({
+   name: String,
+   mfg: Date,
+   category: String,
+   color: String,
+   manufacture: String,
+   detail: String,
+});
+const ToyModel = mongoose.model('toy', ToySchema, 'toy');
+module.exports = ToyModel;
